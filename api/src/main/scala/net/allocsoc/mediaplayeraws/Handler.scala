0c2212dd5d7a077a31b1f259570b6c5ee4242768
Jordi <kozko2001@gmail.com>
2019-02-27 21:49:15 +0100

    Add s3 client test

--- api/src/main/scala/net/allocsoc/mediaplayeraws/Handler.scala
+++ api/src/main/scala/net/allocsoc/mediaplayeraws/Handler.scala
@@ -2,19 +2,19 @@ package net.allocsoc.mediaplayeraws
 
 import com.amazonaws.services.lambda.runtime.events.{ APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent }
 import com.amazonaws.services.lambda.runtime.{ Context, RequestHandler }
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.{ AmazonS3, AmazonS3Client, AmazonS3ClientBuilder }
 import scala.collection.JavaConverters._
 import play.api.libs.json._
 import collection.JavaConverters._
 
 class Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
-  implicit val infoResponseFormat = Json.format[InfoRespon
-    se]
+  implicit val infoResponseFormat = Json.format[InfoResponse]
+  var s3Client = AmazonS3ClientBuilder.defaultClient()
 
   def handleRequest(req: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
     val logger = context.getLogger()
 
-    val infoResponse = InfoResponse(listOfFilesInBucket())
+    val infoResponse = InfoResponse(handlerInfo.listOfFilesInBucket())
 
     return new APIGatewayProxyResponseEvent()
       .withBody(Json.toJson(infoResponse).toString())
@@ -22,12 +22,17 @@ class Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProx
       .withHeaders(Map("Content-Type" -> "text/html; charset=utf-8").asJava)
   }
 
+  def handlerInfo() = new HandlerInfoJson(s3Client)
+
+}
+
+class HandlerInfoJson(s3Client: AmazonS3) {
+
   def listOfFilesInBucket() = {
     val bucketName = "media-player-aws-kzk"
     val prefix = "music/"
 
-    val s3 = AmazonS3ClientBuilder.defaultClient();
-    val objects = s3.listObjects(bucketName, prefix)
+    val objects = s3Client.listObjects(bucketName, prefix)
 
     objects.getObjectSummaries.asScala .map(_.getKey) .toList
   }
--- api/src/test/scala/net/allocsoc/mediaplayeraws/HandlerSpec.scala
+++ api/src/test/scala/net/allocsoc/mediaplayeraws/HandlerSpec.scala
@@ -2,11 +2,14 @@ package net.allocsoc.mediaplayeraws
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.{ ObjectListing, S3ObjectSummary }
 import org.scalatest.{ Matchers, WordSpec }
 import org.scalatest.mockito.MockitoSugar
+import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
 import play.api.libs.json._
-
+import scala.collection.JavaConverters._
 
 
 class HandlerSpec extends WordSpec with MockitoSugar with Matchers  {
@@ -16,15 +19,36 @@ class HandlerSpec extends WordSpec with MockitoSugar with Matchers  {
 
     when(req.getPath) thenReturn "info.json"
 
-    ".handleRequest response has a couple of media files" in {
-      val response = new Handler().handleRequest(req, context)
+    "the s3 bucket has 2 files" when {
+      val s3Client = mock[AmazonS3Client]
+      val objectListing = mock[ObjectListing]
+      val obj1 = HandlerSpec.objectCreate("CelineDion-Titanic.mp3")
+      val obj2 = HandlerSpec.objectCreate("LaMacarena")
 
-      implicit val infoResponseFormat = Json.format[InfoResponse]
-      val model = Json.fromJson[InfoResponse](Json.parse(response.getBody))
+      when(s3Client.listObjects(anyString(), anyString())) thenReturn objectListing
+      when(objectListing.getObjectSummaries) thenReturn (obj1 :: obj2 :: Nil) .asJava
 
-      response.getStatusCode shouldBe 200
-      model.get.files should have size 2
-      model.get.files{0} shouldBe "CelineDion-Titanic.mp3"
+      ".handleRequest response has a couple of media files" in {
+        val handler = new Handler()
+        handler.s3Client = s3Client
+
+        val response = handler.handleRequest(req, context)
+
+        implicit val infoResponseFormat = Json.format[InfoResponse]
+        val model = Json.fromJson[InfoResponse](Json.parse(response.getBody))
+
+        response.getStatusCode shouldBe 200
+        model.get.files should have size 2
+        model.get.files{0} shouldBe "CelineDion-Titanic.mp3"
+      }
     }
+  }
+}
+
+object HandlerSpec {
+  def objectCreate(filename: String): S3ObjectSummary = {
+    val s3Object = new S3ObjectSummary()
+    s3Object.setKey(filename)
+    return s3Object
   }
 }
