bb0bc4a72e72b08c82380d62be208bad92c0bd78
Jordi <kozko2001@gmail.com>
2019-03-16 16:17:22 +0100

    ADD list and prepare to use downloadManager

--- mobile/lib/info.dart
+++ mobile/lib/info.dart
@@ -1,10 +1,10 @@
 class Info {
-  final List<dynamic> files;
+  final List<String> files;
 
   Info(this.files);
 
   Info.fromJson(Map<String, dynamic> json)
-      : files = json['files'];
+      : files = json['files'].cast<String>();
 
   Map<String, dynamic> toJson() =>
     {
